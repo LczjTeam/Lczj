813dc065778c563555cd00eb13ab5de77ee20c20
14260 <1426079590@qq.com>
2018-06-11 12:03:03 +0800

    ok

---
+++ src/main/java/jx/lczj/model/T_admin.java
@@ -0,0 +1,55 @@
+package jx.lczj.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by 14260 on 2018/6/10.
+ */
+public class T_admin implements Serializable{
+    private String admin;  //   用户账号
+    private String name;   //   用户名
+    private String pwd ;   //   密码 默认 '123456',
+    private String isvalid;//  是否有效  默认 '1',
+
+    public String getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(String admin) {
+        this.admin = admin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getIsvalid() {
+        return isvalid;
+    }
+
+    public void setIsvalid(String isvalid) {
+        this.isvalid = isvalid;
+    }
+
+    @Override
+    public String toString() {
+        return "T_admin{" +
+                "admin='" + admin + '\'' +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", isvalid='" + isvalid + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/jx/lczj/model/T_menu.java
@@ -0,0 +1,95 @@
+package jx.lczj.model;
+
+/**
+ * Created by 14260 on 2018/6/10.
+ */
+public class T_menu {
+
+    private String menu;    //编号  not null,
+    private String title;   //名称 not null,
+    private String parents; //父节点 default '0',
+    private String url;     //路径 default '',
+    private String method;  //方法 default '',
+    private int sort_no;    //序号 default 0,
+    private int visible;    //是否可见 default 1,
+    private String css;     //样式 default '',
+
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getParents() {
+        return parents;
+    }
+
+    public void setParents(String parents) {
+        this.parents = parents;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public int getSort_no() {
+        return sort_no;
+    }
+
+    public void setSort_no(int sort_no) {
+        this.sort_no = sort_no;
+    }
+
+    public int getVisible() {
+        return visible;
+    }
+
+    public void setVisible(int visible) {
+        this.visible = visible;
+    }
+
+    public String getCss() {
+        return css;
+    }
+
+    public void setCss(String css) {
+        this.css = css;
+    }
+
+    @Override
+    public String toString() {
+        return "T_menu{" +
+                "menu='" + menu + '\'' +
+                ", title='" + title + '\'' +
+                ", parents='" + parents + '\'' +
+                ", url='" + url + '\'' +
+                ", method='" + method + '\'' +
+                ", sort_no=" + sort_no +
+                ", visible=" + visible +
+                ", css='" + css + '\'' +
+                '}';
+    }
+}
