22990f8ddde0688d50e2ccb894af6e16fa6c1173
14260 <1426079590@qq.com>
2018-06-11 12:02:31 +0800

    ok

---
+++ src/main/java/jx/lczj/controller/AdminController.java
@@ -0,0 +1,36 @@
+package jx.lczj.controller;
+
+import jx.lczj.service.AdminService;
+import jx.lczj.viewmodel.AdminVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.SystemEnvironmentPropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by 14260 on 2018/6/10.
+ */
+
+
+@Controller
+@RequestMapping("admin")
+public class AdminController {
+
+
+    @Autowired
+    AdminService adminService;
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public AdminVo Login(String admin, String pwd, HttpSession session){
+        //System.out.println(admin+"::"+pwd);
+        return adminService.login(admin,pwd,session);
+    }
+
+}
