1b0c000e5323e41445696b00105f7ef4565d984c
14260 <1426079590@qq.com>
2018-06-12 12:06:47 +0800

    ok

---
+++ src/main/webapp/Backstage/custom-js/login.js
@@ -0,0 +1,72 @@
+/**
+ * Created by 14260 on 2018/6/12.
+ */
+$(document).ready(function() {
+
+    $("#btn_login").click(function(){
+        $("#loding-login").css('display','block');
+        var int = setInterval(function(e){
+
+            var params={};
+            params.admin =$("#admin").val();
+            params.pwd =$("#pwd").val();
+            $.ajax({
+                async: false,
+                type: "POST",
+                url: "../admin/login",//注意路径
+                data: params,
+                dataType: "json",
+                success: function (data) {
+                    console.log(JSON.stringify(data,null,4));
+                    if(data.t_admin == null){
+                        swal({
+                            title: "账号不存在或无效！",
+                            text: "",
+                            type: "warning",
+                            allowOutsideClick: true,
+                            showConfirmButton: true,
+                            showCancelButton: false,
+                            confirmButtonClass: "btn-danger",
+                            confirmButtonText: "OK",
+                        });
+                    }else if (data.menuVos==null){
+                        swal({
+                            title: "密码错误！",
+                            text: "",
+                            type: "error",
+                            allowOutsideClick: true,
+                            showConfirmButton: true,
+                            showCancelButton: false,
+                            confirmButtonClass: "btn-danger",
+                            confirmButtonText: "OK",
+                        });
+
+                    }else{
+                        window.location.href="index.jsp";
+                    }
+                },
+                error: function (data) {
+                    console.log(JSON.stringify(data,null,4));
+                    swal({
+                        title: "登陆出错！",
+                        text: "",
+                        type: "error",
+                        allowOutsideClick: true,
+                        showConfirmButton: true,
+                        showCancelButton: false,
+                        confirmButtonClass: "btn-danger",
+                        confirmButtonText: "OK",
+                    });
+                }
+            });
+            $("#loding-login").css('display','none');
+            clearInterval(int);
+
+
+        },1000);
+
+    });
+
+
+
+});
