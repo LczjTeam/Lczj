660140f6cb03084eba763e5610742b792dbdee09
25778 <2577817562@qq.com>
2018-06-18 22:04:30 +0800

    20180618

---
+++ src/main/java/jx/lczj/dao/BrandDao.java
@@ -0,0 +1,26 @@
+package jx.lczj.dao;
+import jx.lczj.model.T_brand;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+/**
+ * Created by 25778 on 2018/6/18.
+ */
+public interface BrandDao {
+    @Select("select * from T_BRAND")
+    public List<T_brand> loadList();
+
+    @Update("update T_BRAND set name= #{1} , company = #{2} where brand = #{0}")
+    boolean update(int brand, String name, String company);
+
+    @Delete("delete from T_BRAND WHERE brand = #{0}")
+    public boolean delete(String brand);
+
+    @Insert("insert into T_BRAND(brand,name,company) values(#{0},#{1},#{2})")
+    public boolean add(int brand, String name, String company);
+
+}
