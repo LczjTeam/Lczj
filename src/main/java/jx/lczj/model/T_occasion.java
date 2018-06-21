ac44ad49148a81d60bbb6bc5886d45c8cbbaa08e
14260 <1426079590@qq.com>
2018-06-21 17:27:22 +0800

    20180615

--- src/main/java/jx/lczj/model/T_occasion.java
+++ src/main/java/jx/lczj/model/T_occasion.java
@@ -2,9 +2,10 @@ package jx.lczj.model;
 
 import java.io.Serializable;
 
-public class T_occasion implements Serializable{
+public class T_occasion implements Serializable {
     private int occasion;                 //场景编号
     private String name;                  //场景名称
+    private String photo;                  //英文名称
 
     public int getOccasion() {
         return occasion;
@@ -22,11 +23,20 @@ public class T_occasion implements Serializable{
         this.name = name;
     }
 
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
     @Override
     public String toString() {
         return "T_occasion{" +
                 "occasion=" + occasion +
                 ", name='" + name + '\'' +
+                ", photo='" + photo + '\'' +
                 '}';
     }
 }
