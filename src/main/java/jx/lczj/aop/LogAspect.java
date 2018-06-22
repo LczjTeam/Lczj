ef9e4840dd86021f82ecda14af2d046e22e17d94
14260 <1426079590@qq.com>
2018-06-22 18:05:38 +0800

    20180622

--- src/main/java/jx/lczj/aop/LogAspect.java
+++ src/main/java/jx/lczj/aop/LogAspect.java
@@ -35,73 +35,48 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 登录检测
+ */
 @Aspect
 @Component
 public class LogAspect {
 
     // 任何通知方法都可以将第一个参数定义为 org.aspectj.lang.JoinPoint类型
-
     //@Pointcut("execution(* jx.lczj.controller..*.*(..))")
     @Pointcut("@annotation(jx.lczj.anotation.SysLogin)")
     private void controllerMethod(){}//定义一个切入点
 
-
     @Before("controllerMethod()")
     public void doBefore(JoinPoint call){
-
         String clazz =call.getTarget().getClass().getName();
-
         // 获取目标对象上正在执行的方法名
-
         String methodName =call.getSignature().getName();
-
-         System.out.println("前置通知:" + clazz + "类的" + methodName + "方法开始了...");
-
-
-
+        System.out.println(clazz + "类的" + methodName + "方法开始了...");
     }
-
-
-
-
 
     @AfterReturning("controllerMethod()")
     public void doAfter(){
-
-        System.out.println("-------------执行结束----------------");
-
+        System.out.println("结束......");
     }
-
-
 
     @AfterThrowing("controllerMethod()")
 
     public void doAfterThrow(){
 
-        System.out.println("例外通知");
-
+        System.out.println("出错啦....");
     }
-
-
 
     @Around("controllerMethod()")
     public Object doBasicProfiling(ProceedingJoinPoint pjp) throws Throwable{
-
-        System.out.println("-------------执行开始----------------");
-
+        System.out.println("开始.....");
         HttpServletRequest request =((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-
         HttpSession session =request.getSession();
-
         if(session.getAttribute("admin")==null){
             return  false;
         }
-
         Object object = pjp.proceed();//执行该方法
-
-        System.out.println("-------------执行成功----------------");
-
+        System.out.println("成功.....");
         return object;
-
     }
 }
--- src/main/java/jx/lczj/aop/UserDivAspect.java
+++ src/main/java/jx/lczj/aop/UserDivAspect.java
@@ -19,62 +19,36 @@ import javax.servlet.http.HttpSession;
 public class UserDivAspect {
 
     // 任何通知方法都可以将第一个参数定义为 org.aspectj.lang.JoinPoint类型
-
     //@Pointcut("execution(* jx.lczj.controller..*.*(..))")
     @Pointcut("@annotation(jx.lczj.anotation.UserDivOperation)")
     private void controllerMethod(){}//定义一个切入点
-
 
     @Before("controllerMethod()")
     public void doBefore(JoinPoint call){
 
         String clazz =call.getTarget().getClass().getName();
-
         // 获取目标对象上正在执行的方法名
-
         String methodName =call.getSignature().getName();
-
-         System.out.println("前置通知:" + clazz + "类的" + methodName + "方法开始了...");
-
-
-
+        System.out.println(clazz + "类的" + methodName + "方法开始了...");
     }
-
-
-
-
 
     @AfterReturning("controllerMethod()")
     public void doAfter(){
-
-        System.out.println("-------------执行结束----------------");
-
+        System.out.println("结束....");
     }
-
-
 
     @AfterThrowing("controllerMethod()")
-
     public void doAfterThrow(){
-
-        System.out.println("例外通知");
-
+        System.out.println("出错啦....");
     }
-
-
 
     @Around("controllerMethod()")
     public Object doBasicProfiling(ProceedingJoinPoint pjp) throws Throwable{
-
-
-        System.out.println("-------------执行开始----------------");
-
+        System.out.println("开始....");
         HttpServletRequest request =((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-
         HttpSession session =request.getSession();
-
         if(session.getAttribute("admin")==null){
-            System.out.println("-------------你尚未登录----------------");
+            System.out.println("你尚未登录....");
             return "你尚未登录";
         }else{
             boolean ok = false;
@@ -91,11 +65,11 @@ public class UserDivAspect {
                 }
             }
             if(!ok){
-                System.out.println("-------------权限不足----------------");
+                System.out.println("权限不足....");
                 return "权限不足！！";
             }
             Object object = pjp.proceed();//执行该方法
-            System.out.println("-------------执行成功----------------");
+            System.out.println("成功....");
             return object;
         }
 
