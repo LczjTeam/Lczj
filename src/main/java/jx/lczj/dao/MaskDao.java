c0751a547fa635fb19bb2490ae3819e99c59b60c
DDLD <747413073@qq.com>
2018-07-05 11:28:50 +0800

    2018.6.25  20：29

---
+++ src/main/java/jx/lczj/dao/MaskDao.java
@@ -0,0 +1,24 @@
+package jx.lczj.dao;
+
+import jx.lczj.model.T_mask;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface MaskDao {
+
+    @Select("select * from T_MASK")
+    List<T_mask> loadList();
+
+    @Insert("insert into T_mask values(#{0},#{1})")
+    boolean add(int mask, String name);
+
+    @Delete("delete T_mask where MASK = #{0}")
+    boolean delete(String mask);
+
+    @Update("update T_mask set MASK = #{0}, NAME = #{1}")
+    boolean update(int mask, String name);
+}
