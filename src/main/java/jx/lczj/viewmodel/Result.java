5c5fc8315f96afa826a562ea83646c5cf545868c
14260 <1426079590@qq.com>
2018-07-14 09:19:13 +0800

    Fronts 20180708

---
+++ src/main/java/jx/lczj/viewmodel/Result.java
@@ -0,0 +1,33 @@
+package jx.lczj.viewmodel;
+
+/**
+ * Created by 14260 on 2018/7/11.
+ */
+public class Result {
+    private int code;
+    private String detail;
+
+    public Result() {
+    }
+
+    public Result(int code, String detail) {
+        this.code = code;
+        this.detail = detail;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
