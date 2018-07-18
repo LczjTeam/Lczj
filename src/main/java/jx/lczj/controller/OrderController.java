6593f084c6877181af3234d1499c939b98540942
XIAO-e <2182772477@qq.com>
2018-07-18 17:35:45 +0800

    订单更新接口 xiaoyi 2018.7.18

--- src/main/java/jx/lczj/controller/OrderController.java
+++ src/main/java/jx/lczj/controller/OrderController.java
@@ -1,27 +1,44 @@
 package jx.lczj.controller;
 
-import jx.lczj.model.T_order;
 import jx.lczj.service.OrderService;
-import jx.lczj.viewmodel.OrderVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
-/**
- * Created by WuLinZhi on 2018-07-10.
- */
 @Controller
-@RequestMapping("/order")
+@RequestMapping("order")
 public class OrderController {
+
     @Autowired
     private OrderService orderService;
 
-    @RequestMapping("/list")
+
+    /**
+     * 更新商品星级，服务星级，快递星级，评语
+     * @param order
+     * @param goodsstar
+     * @param servicestar
+     * @param expressstar
+     * @param comments
+     * @return
+     */
+    @RequestMapping("/update1")
     @ResponseBody
-    public List<OrderVo> loadOrders(){
-        return orderService.loadOrders();
+    public boolean update1(String order,int goodsstar,int servicestar,int expressstar,String comments){
+        return orderService.update1(order,goodsstar,servicestar,expressstar,comments);
+    }
+
+    /**
+     * 更新快递名，运单号
+     * @param order
+     * @param express
+     * @param expressid
+     * @return
+     */
+    @RequestMapping("/update2")
+    @ResponseBody
+    public boolean update2(String order,String express,String expressid){
+        return orderService.update2(order,express,expressid);
     }
 }
