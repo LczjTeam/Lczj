faf920a1bdc98eab486e2149ac2946f6d5a2e23e
DDLD <747413073@qq.com>
2018-07-20 19:18:20 +0800

    2018.7.20 19.18

--- src/main/java/jx/lczj/controller/OrderController.java
+++ src/main/java/jx/lczj/controller/OrderController.java
@@ -1,10 +1,15 @@
 package jx.lczj.controller;
 
+import jx.lczj.model.T_order;
 import jx.lczj.service.OrderService;
+import jx.lczj.viewmodel.OrderCreateVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.ParseException;
+import java.util.List;
 
 @Controller
 @RequestMapping("order")
@@ -28,6 +33,12 @@ public class OrderController {
     public boolean update1(String order,int goodsstar,int servicestar,int expressstar,String comments){
         return orderService.update1(order,goodsstar,servicestar,expressstar,comments);
     }
+    /**
+     * 获取全部订单
+     */
+    @RequestMapping("/list")
+    @ResponseBody
+    public List<T_order> list(){return orderService.list();};
 
     /**
      * 更新快递名，运单号
@@ -41,4 +52,30 @@ public class OrderController {
     public boolean update2(String order,String express,String expressid){
         return orderService.update2(order,express,expressid);
     }
+    /**
+     * 通过时间查找订单号
+     */
+    @RequestMapping("/seachByTime")
+    @ResponseBody
+    public List<T_order> seachByTime(String start ,String end) throws ParseException {
+
+        System.out.println(start+end);
+        return orderService.seachByTime(start,end);
+    };
+    /**
+     * 通过订单号查找
+     */
+    @RequestMapping("/seachById")
+    @ResponseBody
+    public T_order loadById(String order){
+        System.out.println(orderService.loadById(order));
+        return orderService.loadById(order);}
+
+    /**
+     * 更新总价
+     */
+    @RequestMapping("/update3")
+    @ResponseBody
+    public boolean update3(String order,String price){
+        return orderService.update3(order,price);}
 }
--- src/main/java/jx/lczj/service/OrderService.java
+++ src/main/java/jx/lczj/service/OrderService.java
@@ -1,14 +1,14 @@
 package jx.lczj.service;
 
 import jx.lczj.dao.*;
-import jx.lczj.model.T_mywear;
-import jx.lczj.model.T_order;
-import jx.lczj.model.T_orderdetail;
-import jx.lczj.model.T_wearglass;
-import jx.lczj.viewmodel.OrderVo;
+import jx.lczj.model.*;
+import jx.lczj.viewmodel.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,5 +42,54 @@ public class OrderService {
      */
     public boolean update2(String order, String express, String expressid) {
         return orderDao.update2(order,express,expressid);
+    }
+
+    /**
+     * 获取全部订单
+     * @return
+     */
+    public List<T_order> list() {
+        return orderDao.list();
+    }
+
+    /**
+     * 通过时间查找
+     */
+    public List<T_order> seachByTime(String start, String end) throws ParseException {
+
+        System.out.println(start+" 00:00:00"+end+" 23:59:59");
+
+        java.util.Date start_time = new java.util.Date();
+        java.util.Date end_time = new java.util.Date();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        try{
+            start_time = sdf.parse(start);
+            end_time = sdf.parse(end);
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
+
+        int i = start_time.compareTo(end_time);
+        if (i > 0){
+            System.out.println("start_time:"+start_time);
+            System.out.println("end_time:"+end_time);
+        }
+        List<T_order> t_order = orderDao.seachByTime(start_time,end_time);
+        return t_order ;
+    }
+    //通过订单号查找
+    public T_order loadById(String order){
+        System.out.println("order:"+order);
+        return orderDao.listById(order);
+    }
+
+    /**
+     *更新总价
+     * @return
+     */
+    public boolean update3(String order, String price) {
+    return orderDao.update3(order,price);
     }
 }
